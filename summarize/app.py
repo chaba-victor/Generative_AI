80446463c95d984cabb44c4cea08459337547c61
ShubhamZoro <89676909+ShubhamZoro@users.noreply.github.com>
2024-03-03 18:10:39 +0530

    Update app.py

--- summarize/app.py
+++ summarize/app.py
@@ -9,8 +9,8 @@ model = PeftModel.from_pretrained(original_model, "prompt")
 import streamlit as st
 
 
-st.title('Skimlit')
-txt=st.text_area('Text to analyze', '''Wikis are enabled by wiki software, otherwise known as wiki engines. A wiki engine, being a form of a content management system, differs from other web-based systems such as blog software, in that the content is created without any defined owner or leader, and wikis have little inherent structure, allowing structure to emerge according to the needs of the users.[1] Wiki engines usually allow content to be written using a simplified markup language and sometimes edited with the help of a rich-text editor.[2] There are dozens of different wiki engines in use, both standalone and part of other software, such as bug tracking systems. Some wiki engines are free and open-source, whereas others are proprietary. Some permit control over different functions (levels of access); for example, editing rights may permit changing, adding, or removing material. Others may permit access without enforcing access control. Further rules may be imposed to organize content.''')
+st.title('Summarize')
+txt=st.text_area('Text to analyze', '''''')
 
 # Load saved model
 def get_results(abs_text):
@@ -37,5 +37,5 @@ def get_results(abs_text):
 
 
 print("\n\nAI formatted abstract is given below:\n")
-if st.button('skimmed'):
+if st.button('summarize'):
   get_results(txt)
