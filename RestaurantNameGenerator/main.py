63620647d7a00ed54c9f0b7e97b690ae5f791daa
ShubhamZoro <89676909+ShubhamZoro@users.noreply.github.com>
2024-03-23 13:52:00 +0530

    Update main.py

--- RestaurantNameGenerator/main.py
+++ RestaurantNameGenerator/main.py
@@ -5,7 +5,7 @@ import LangChain
 
 if cuisine:
     response=LangChain.generate_restaurant_name_and_items(cuisine)
-    st.header(response['restaurant_name'][3:len(response['restaurant_name'])-1])
+    st.header(response['restaurant_name'])
     menu_items=response['menu_items'].strip().split(",")
     st.write('**Menu Items**')
     for item in menu_items:
