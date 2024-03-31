9213ca3f181627785f7c429180816e5ae3c5100a
ShubhamZoro <shubhamshekhar19@gmail.com>
2024-03-31 13:55:24 +0530

    Chatwithurl

--- ChatwithWebsite/main.py
+++ ChatwithWebsite/main.py
@@ -1,16 +1,97 @@
-# This is a sample Python script.
+import streamlit as st
+from langchain_core.messages import AIMessage, HumanMessage
+from langchain_community.document_loaders import WebBaseLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from dotenv import load_dotenv
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+api_key=st.secrets['api_key']
+def get_resposne(user_input):
+    retriever_chain = get_context_retriever_chain(st.session_state.vector_store)
+    conversation_rag_chain = get_conversational_rag_chain(retriever_chain)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    response = conversation_rag_chain.invoke({
+        "chat_history": st.session_state.chat_history,
+        "input": user_input
+    })
+
+    return response['answer']
+
+def get_vectorstore_from_url(url):
+    #get the text document forms
+    loader=WebBaseLoader(url)
+    documents=loader.load()
+    text_splitter=RecursiveCharacterTextSplitter()
+    document_chunks=text_splitter.split_documents(documents)
+    #vector_store
+    vector_store=Chroma.from_documents(document_chunks,OpenAIEmbeddings(openai_api_key=api_key))
+    return vector_store
+
+def get_context_retriever_chain(vector_store):
+    llm = ChatOpenAI(openai_api_key=api_key)
+    retriever = vector_store.as_retriever()
+    prompt = ChatPromptTemplate.from_messages([
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("user", "{input}"),
+        ("user",
+         "Given the above conversation, generate a search query to look up in order to get information relevant to the conversation")
+    ])
+    retriever_chain = create_history_aware_retriever(llm, retriever, prompt)
+    return retriever_chain
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def get_conversational_rag_chain(retriever_chain):
+    llm = ChatOpenAI(openai_api_key=api_key)
+
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", "Answer the user's questions based on the below context:\n\n{context}"),
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("user", "{input}"),
+    ])
+
+    stuff_documents_chain = create_stuff_documents_chain(llm, prompt)
+
+    return create_retrieval_chain(retriever_chain, stuff_documents_chain)
+# app config
+
+st.set_page_config(page_title="chat with website")
+st.title("chat with website")
+
+#sidebar
+with st.sidebar:
+    st.header("setting")
+    website_url=st.text_input("website url")
+if website_url is None or website_url=="":
+    st.info("Enter the url")
+else:
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = [
+            AIMessage(content="Hello, I am a bot. How can I help you?"),
+        ]
+    #create coversation chain
+    if "vector_store" not in st.session_state:
+        st.session_state.vector_store = get_vectorstore_from_url(website_url)
+    retriever_chain=get_context_retriever_chain(st.session_state.vector_store)
+
+    #user input
+    user_query=st.chat_input("Type your message here--")
+    if user_query:
+        response=get_resposne(user_query)
+        st.session_state.chat_history.append(HumanMessage(content=user_query))
+        st.session_state.chat_history.append(AIMessage(content=response))
+
+    for message in st.session_state.chat_history:
+        if isinstance(message,AIMessage):
+            with st.chat_message("AI"):
+                st.write(message.content)
+        elif isinstance(message,HumanMessage):
+            with st.chat_message("Human"):
+                st.write(message.content)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
